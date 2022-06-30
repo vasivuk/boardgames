a9a7a2bdb2110bebef0ccc21fb8195dbc91e120d
vasivuk <vasivuk@yahoo.com>
2022-06-30 13:01:40 +0200

    Initial commit

---
+++ src/main/java/com/vasivuk/boardgames/BoardgamesApplication.java
@@ -0,0 +1,13 @@
+package com.vasivuk.boardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasivuk/boardgames/BoardgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasivuk.boardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
