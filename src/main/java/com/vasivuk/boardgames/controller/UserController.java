87551604f70c0a5afff87f5a98aa44a62407fd36
vasivuk <vasivuk@yahoo.com>
2022-07-05 18:52:57 +0200

    Fixed refresh token being invalid

--- src/main/java/com/vasivuk/boardgames/controller/UserController.java
+++ src/main/java/com/vasivuk/boardgames/controller/UserController.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vasivuk.boardgames.configuration.Constants;
 import com.vasivuk.boardgames.model.AppUser;
 import com.vasivuk.boardgames.service.impl.UserService;
 import lombok.RequiredArgsConstructor;
@@ -61,7 +62,7 @@ public class UserController {
 
                 String refresh_token = authorizationHeader.substring("Bearer ".length());
 
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                Algorithm algorithm = Algorithm.HMAC256(Constants.SECRET.getBytes());
                 JWTVerifier verifier = JWT.require(algorithm).build();
                 DecodedJWT decodedJWT = verifier.verify(refresh_token);
                 String username = decodedJWT.getSubject();
@@ -69,13 +70,12 @@ public class UserController {
 
                 String access_token = JWT.create()
                         .withSubject(user.getEmail())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withExpiresAt(new Date(System.currentTimeMillis() + Constants.ACCESS_TOKEN_EXPIRATION))
                         .withClaim("authority", user.getUserRole().getName())
                         .sign(algorithm);
 
                 Map<String, String> tokens = new HashMap<>();
                 tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
                 response.setContentType(MediaType.APPLICATION_JSON_VALUE);
                 new ObjectMapper().writeValue(response.getOutputStream(), tokens);
             } catch (Exception exception) {
